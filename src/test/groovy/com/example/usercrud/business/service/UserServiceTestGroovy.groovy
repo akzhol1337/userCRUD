1b6ede362f36f640c6e85f551d98784157f20046
Akzhol Issaliyev <akzholbusiness@gmail.com>
2022-04-25 05:39:28 +0600

    tests update

--- src/test/groovy/com/example/usercrud/business/service/UserServiceTestGroovy.groovy
+++ src/test/groovy/com/example/usercrud/business/service/UserServiceTestGroovy.groovy
@@ -1,77 +1,175 @@
-import com.example.usercrud.business.service.UserService
+package com.example.usercrud.business.service
+
+import com.example.usercrud.business.entity.User
 import com.example.usercrud.persistance.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.web.client.RestTemplate
+import spock.lang.Execution
 import spock.lang.Specification
+import spock.lang.Stepwise
 
-class UserServiceTestGroovy extends Specification{
-    @Autowired
-    UserService userServiceTest
-    @Autowired
-    UserRepository userRepoTest
+@DataJpaTest
+class UserServiceTestGroovy extends Specification {
+    private static final testUser = new User(1L, "firstName", "lastName", "middleName", "country", 1, "email@email.email")
 
-    // todo
-
-
-    def "It should return false for deleting un-existing user by id"() {
+    def "should return false for deleting not existing user by ID"() {
         setup:
-            def userServiceTest = new UserService()
-            def id = -1L
+        def restTemplate = Mock(RestTemplate)
+        def userRepository = Mock(UserRepository)
+        userRepository.existsById(1L) >> false
+        def userService = new UserService(userRepository, restTemplate)
+
         when:
-            Boolean answer = userServiceTest.deleteById(id)
+        def answer = userService.deleteById(1L)
+
         then:
-            !answer
+        !answer
     }
 
-    def "It should return true for deleting existing user by id"() {
+    def "should return true for deleting existing user by ID"() {
         setup:
-            def user = new User(1L, "firstName", "secondName", "middleName", "country", 1, "email@emailemail")
-            userRepoTest.save(user)
+        def restTemplate = Mock(RestTemplate)
+        def userRepository = Mock(UserRepository)
+        userRepository.existsById(1L) >> true
+        def userService = new UserService(userRepository, restTemplate)
+
         when:
-            Boolean answer = userServiceTest.deleteById(1L)
+        def answer = userService.deleteById(1L)
+
         then:
-            answer
+        answer
     }
 
-    def "It should return false for deleting un-existing user by email"() {
+    def "should return false for deleting not existing user by email"() {
         setup:
-            def userServiceTest = new UserService()
-            def email = "email@email.email"
+        def restTemplate = Mock(RestTemplate)
+        def userRepository = Mock(UserRepository)
+        userRepository.existsByEmail("email@email.email") >> false
+        def userService = new UserService(userRepository, restTemplate)
+
         when:
-            Boolean answer = userServiceTest.deleteByEmail(email)
+        def answer = userService.deleteByEmail("email@email.email")
+
         then:
-            !answer
+        !answer
     }
 
-    def "It should return true for deleting existing user by email"() {
+    def "should return true for deleting existing user by email"() {
         setup:
-            def user = new User(1L, "firstName", "secondName", "middleName", "country", 1, "email@email.email")
-            userRepoTest.save(user)
-        when:
-            Boolean answer = userServiceTest.deleteById("email@email.email")
-        then:
-            answer
-    }
+        def restTemplate = Mock(RestTemplate)
+        def userRepository = Mock(UserRepository)
+        userRepository.existsByEmail("email@email.email") >> true
+        def userService = new UserService(userRepository, restTemplate)
+        userRepository.save(testUser)
 
-    def "It should return false for checking if un-existing user exists by email"() {
-        setup:
-            def email = "email@email.email"
         when:
-            boolean answer = userServiceTest.existsByEmail(email)
-        then:
-            !answer
-    }
+        def answer = userService.deleteByEmail("email@email.email")
 
-    def "It should return true for checking if existing user exists by email"() {
-        setup:
-            def user = new User(1L, "firstName", "secondName", "middleName", "country", 1, "email@email.email")
-            def email = "email@email.email"
-        when:
-            boolean answer = userServiceTest.existsByEmail(email)
         then:
-            answer
+        answer
     }
 
 
+    def "should return false for checking if user exists for not existing user by email"() {
+        setup:
+        def restTemplate = Mock(RestTemplate)
+        def userRepository = Mock(UserRepository)
+        userRepository.existsByEmail("email@email.email") >> false
+        def userService = new UserService(userRepository, restTemplate)
+
+        when:
+        def answer = userService.existsByEmail("email@email.email")
+
+        then:
+        !answer
+    }
+
+    def "should return true for checking if user exists for existing user by email"() {
+        setup:
+        def restTemplate = Mock(RestTemplate)
+        def userRepository = Mock(UserRepository)
+        userRepository.existsByEmail("email@email.email") >> true
+        def userService = new UserService(userRepository, restTemplate)
+        userRepository.save(testUser)
+
+        when:
+        def answer = userService.existsByEmail("email@email.email")
+
+        then:
+        answer
+    }
+
+    def "should update not existing user by email"() {
+        setup:
+        def restTemplate = Mock(RestTemplate)
+        def userRepository = Mock(UserRepository)
+        userRepository.findByEmail("email@email.email") >> Optional.empty()
+        def userService = new UserService(userRepository, restTemplate)
+
+        when:
+        def updatedUser = userService.updateByEmail("email@email.email", testUser)
+
+        then:
+        updatedUser.isEmpty()
+    }
 
 
+
+    def "should update existing user by email"() {
+        setup:
+        def restTemplate = Mock(RestTemplate)
+        def userRepository = Mock(UserRepository)
+        userRepository.findByEmail("email@email.email") >> Optional.of(testUser)
+        def userService = new UserService(userRepository, restTemplate)
+        def newUser = new User(1L, "newFirstName", "newLastName", "newMiddleName", "newCountry", 2, "newEmail@email.email")
+
+        when:
+        def updatedUser = userService.updateByEmail("email@email.email", newUser)
+
+        then:
+        updatedUser.isPresent()
+        updatedUser.get().getFirstName() == "newFirstName"
+        updatedUser.get().getLastName() == "newLastName"
+        updatedUser.get().getMiddleName() == "newMiddleName"
+        updatedUser.get().getEmail() == "newEmail@email.email"
+        updatedUser.get().getCountry() == "newCountry"
+        updatedUser.get().getGender() == 2
+    }
+
+
+    def "should update not existing user by id"() {
+        setup:
+        def restTemplate = Mock(RestTemplate)
+        def userRepository = Mock(UserRepository)
+        userRepository.findById(1L) >> Optional.empty()
+        def userService = new UserService(userRepository, restTemplate)
+
+        when:
+        def updatedUser = userService.updateById(1L, testUser)
+
+        then:
+        updatedUser.isEmpty()
+    }
+
+    def "should update existing user by id"() {
+        setup:
+        def restTemplate = Mock(RestTemplate)
+        def userRepository = Mock(UserRepository)
+        userRepository.findById(1L) >> Optional.of(testUser)
+        def userService = new UserService(userRepository, restTemplate)
+        def newUser = new User(null, "newFirstName", "newLastName", "newMiddleName", "newCountry", 2, "newEmail@email.email")
+
+        when:
+        def updatedUser = userService.updateById(1L, newUser)
+
+        then:
+        updatedUser.isPresent()
+        updatedUser.get().getFirstName() == "newFirstName"
+        updatedUser.get().getLastName() == "newLastName"
+        updatedUser.get().getMiddleName() == "newMiddleName"
+        updatedUser.get().getEmail() == "newEmail@email.email"
+        updatedUser.get().getCountry() == "newCountry"
+        updatedUser.get().getGender() == 2
+    }
 }
--- src/test/groovy/com/example/usercrud/presentation/controller/UserControllerTestGroovy.groovy
+++ src/test/groovy/com/example/usercrud/presentation/controller/UserControllerTestGroovy.groovy
@@ -1,33 +1,152 @@
+package com.example.usercrud.presentation.controller
+
 import com.example.usercrud.business.entity.User
+import com.example.usercrud.business.service.UserService
+import com.example.usercrud.persistance.repository.UserRepository
+import com.example.usercrud.presentation.UserController
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import spock.lang.Specification
+import spock.mock.DetachedMockFactory
 
+import javax.servlet.http.HttpServletRequest
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
+@WebMvcTest(controllers = [UserController])
 class UserControllerTestGroovy extends Specification{
     @Autowired
-    private UserServuce userServiceTest
+    MockMvc mockMvc
     @Autowired
-    private MockMvc mock
+    UserService userService
 
-    // todo
+    private testUser = new User(1L, "firstName", "lastName", "middleName", "country", 1, "email@email.email")
 
-    def "it should return 200 for adding correct user to db"() {
-        given:
-            User user = new User(null, "firstName", "secondName", "middleName", "country", 0, "email@emaill.email")
-        when:
-            mock.perform(post("/user").contentType(MediaType.APPLICATION_JSON).content(asJsonString(user))).andDo(print()).andExpect(status().isOk());
+    private def asJsonString(Object obj){
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    def "it should return 400 for adding incorrect user to db (empty firstname)"() {
-        given:
-            User user = new User(null, null, "secondname", "middlename", "country", 0, "email@email.email");
+    def "should return 200 when adding new user with correct data"() {
+        setup:
+        userService.addUser(_ as User, _ as HttpServletRequest) >> Optional.of(testUser)
+
         when:
-            mock.perform(post("/user").contentType(MediaType.APPLICATION_JSON).content(asJsonString(user))).andDo(print()).andExpect(status().isBadRequest());
+        def response = mockMvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON).content(asJsonString(testUser))).andReturn().response
+
+        then:
+        response.status == HttpStatus.OK.value()
+    }
+
+    def "should return 400 when adding user with empty first name"() {
+        setup:
+        def testIncorrectFirstNameUser = new User(1L, null, "lastName", "middleName","country", 1, "email@email.email")
+
+        when:
+        def response = mockMvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON).content(asJsonString(testIncorrectFirstNameUser))).andReturn().response
+
+        then:
+        response.status == HttpStatus.BAD_REQUEST.value()
+    }
+
+    def "should return 400 when adding user with empty email"() {
+        setup:
+        def testIncorrectEmailUser = new User(1L, "firstName", "lastName", "middleName","country", 1, null)
+
+        when:
+        def response = mockMvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON).content(asJsonString(testIncorrectEmailUser))).andReturn().response
+
+        then:
+        response.status == HttpStatus.BAD_REQUEST.value()
+    }
+
+    def "should return 400 when adding user with incorrect gender"() {
+        setup:
+        testIncorrectGenderUser = new User(1L, "firstName", "lastName", "middleName","country", null, "email@email.email")
+
+        when:
+        def response = mockMvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON).content(asJsonString(testIncorrectEmailUser))).andReturn().response
+
+        then:
+        response.status == HttpStatus.BAD_REQUEST.value()
+    }
+
+    def "should return 400 when adding user with existing email"() {
+        setup:
+        userService.addUser(_ as User, _ as HttpServletRequest) >> Optional.empty()
+
+        when:
+        def response = mockMvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON).content(asJsonString(testUser))).andReturn().response
+
+        then:
+        response.status == HttpStatus.BAD_REQUEST.value()
+    }
+
+    def "should return 200 for getting existing user by id"() {
+        setup:
+        userService.findById(1L) >> Optional.of(testUser)
+
+        when:
+        def response = mockMvc.perform(get("/user/id/1").contentType(MediaType.APPLICATION_JSON)).andReturn().response
+
+        then:
+        response.status == HttpStatus.OK.value()
+    }
+
+    def "should return 400 for getting un existing user by id"() {
+        setup:
+        userService.findById(1L) >> Optional.empty()
+
+        when:
+        def response = mockMvc.perform(get("/user/id/1").contentType(MediaType.APPLICATION_JSON)).andReturn().response
+
+        then:
+        response.status == HttpStatus.BAD_REQUEST.value()
+    }
+
+    def "should return 200 for getting existing user by email"() {
+        setup:
+        userService.findByEmail("email@email.email") >> Optional.of(testUser)
+
+        when:
+        def response = mockMvc.perform(get("/user/email/email@email.email").contentType(MediaType.APPLICATION_JSON)).andReturn().response
+
+        then:
+        response.status == HttpStatus.OK.value()
+    }
+
+    def "should return 400 for getting un existing user by email"() {
+        setup:
+        userService.findByEmail("email@email.email") >> Optional.empty()
+
+        when:
+        def response = mockMvc.perform(get("/user/email/email@email.email").contentType(MediaType.APPLICATION_JSON)).andReturn().response
+
+        then:
+        response.status == HttpStatus.BAD_REQUEST.value()
+    }
+
+    @TestConfiguration
+    static class Config {
+        DetachedMockFactory detachedMockFactory = new DetachedMockFactory()
+        @Bean
+        UserService userService() {
+            return detachedMockFactory.Mock(UserService)
+        }
     }
 
 }
